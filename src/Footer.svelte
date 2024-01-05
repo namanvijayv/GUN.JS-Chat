62b730ba7e03b930d4b56ac46cc5612dfa2bae8f
namanvijayv <namanvijayvargiya007@gmail.com>
2024-01-05 11:26:10 +0530

    Footer Change

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -2,15 +2,9 @@
   Made with
   <span>♥️</span>
   by
-  <a href="https://koenvangilst.nl">Koen van Gilst</a>
+  <a href="https://koenvangilst.nl">Gorilla</a>
   <br />
-  Using
-  <a href="https://svelte.dev/">svelte</a>
-  and
-  <a href="https://gun.eco/">gunDB</a>
-  see
-  <a href="https://github.com/vnglst/svelte-gundb-chat">Github</a>
-  <br />
+  For Motu PANDA
   v. {process.env.APP_VERSION} git {process.env.COMMIT_HASH}
 </footer>
 
